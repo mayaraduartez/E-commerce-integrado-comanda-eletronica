4770d1d527b9a24d13d4d9457255f1e21f7573f2
mayaraduartez <mayaraduart321@gmail.com>
2023-06-18 01:16:41 -0300

    sistema de login completo

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -20,50 +20,52 @@ async function cadastro(req, res) {
 
     res.redirect("/login");
   } catch (error) {
-    res.render("login/cadastro.ejs", { msg: "Esse e-mail já possui cadastro!" });
+    res.render("login/cadastro.ejs", {
+      msg: "Esse e-mail já possui cadastro!",
+    });
   }
 }
-
 
 async function abreTela(req, res, msg = "") {
   res.render("login/login.ejs", { msg: "" });
 }
 
-
 async function cadastrar(req, res) {
-res.render("login/cadastro.ejs", {msg:""});
+  res.render("login/cadastro.ejs", { msg: "" });
 }
 
-async function forgot(req,res) {
-res.render("login/forgot.ejs" , {msg:""});
+async function forgot(req, res) {
+  res.render("login/forgot.ejs", { msg: "" });
 }
 
-async function telatoken(req,res) {
-res.render("login/token.ejs", {msg:""});
+async function telatoken(req, res) {
+  res.render("login/token.ejs", { msg: "" });
 }
 
 async function atualizarsenha(req, res) {
   var novasenha = req.body.novasenha;
   let usuario = await Usuario.findOne({
     where: {
-      email: req.body.email
+      email: req.body.email,
     },
   });
 
   let token = await Token.findOne({
     where: {
-      token: req.body.token
-    }
+      token: req.body.token,
+    },
   });
 
   if (!usuario) {
-    res.render('login/token.ejs', { msg: 'O email digitado está incorreto do email que foi enviado o token!' });
-    return; 
+    res.render("login/token.ejs", {
+      msg: "O email digitado está incorreto do email que foi enviado o token!",
+    });
+    return;
   }
 
   if (!token) {
-    res.render('login/token.ejs', { msg: 'O token está incorreto!' });
-    return; 
+    res.render("login/token.ejs", { msg: "O token está incorreto!" });
+    return;
   }
 
   if (token.UsuarioId === usuario.id) {
@@ -72,92 +74,92 @@ async function atualizarsenha(req, res) {
     var hash = bcrypt.hashSync(novasenha, salt);
     usuario.senha = hash;
     await usuario.save();
-    res.render('login/login.ejs');
-  } 
+    res.render("login/login.ejs");
+  }
 }
 
+async function recuperar(req, res) {
+  if (req.body.email == "") {
+    res.render("login/forgot.ejs", { msg: "Você deve informar um email!" });
+  } else {
+    let usuario = await Usuario.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
-async function recuperar(req,res) {
+    if (!usuario) {
+      res.render("login/forgot.ejs", { msg: "Usuário não cadastrado!" });
+    } else if (usuario.token === "" || usuario.token === null) {
+      let token = generatePassword();
 
-    if(req.body.email == "") {
-      res.render('login/forgot.ejs', {msg: 'Você deve informar um email!'})
-    }
-    else{
-        let usuario = await Usuario.findOne({
+      let savetoken = await Token.create({
+        UsuarioId: usuario.id,
+        token: token,
+        datacriacao: new Date(),
+      });
+
+      enviaremail(usuario, token);
+      res.redirect("/token");
+    } else {
+      await Token.destroy({
         where: {
-        email: req.body.email
-        }
-
-        })
-
-        if(!usuario ){
-          res.render('login/forgot.ejs', {msg: 'Usuário não cadastrado!'})
-        }
-        else if(usuario.token === "" || usuario.token === null){
-          let token = generatePassword();
-
-
-          let savetoken = await Token.create({
           UsuarioId: usuario.id,
-          token: token,
-          datacriacao: new Date()
-        });
+        },
+      });
 
-        
+      let token = generatePassword();
 
-        enviaremail(usuario,token);
-          res.redirect("/token");
+      let savetoken = await Token.create({
+        UsuarioId: usuario.id,
+        token: token,
+        datacriacao: new Date(),
+      });
 
-        } 
-        else {
-          await Token.destroy({
-            where: {
-              UsuarioId: usuario.id
-            }
-          });
-
-          let token = generatePassword();
-
-            let savetoken = await Token.create({
-            UsuarioId: usuario.id,
-            token: token,
-            datacriacao: new Date()
-          });
-          
-          enviaremail(usuario,token);
-            res.redirect("/token");
-        }
-
-        
+      enviaremail(usuario, token);
+      res.redirect("/token");
     }
- }
-
-function generatePassword(){
-const chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var pass = ''
-for(var i=0; i< 10; i++)
-pass += chars.charAt(Math.random() * 61)
-return pass
+  }
 }
 
-function enviaremail(usuario,token){
+function generatePassword() {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var pass = "";
+  for (var i = 0; i < 10; i++) pass += chars.charAt(Math.random() * 61);
+  return pass;
+}
+
+function enviaremail(usuario, token) {
   const email = {
-    from: 'recuperacaodesenhaif@hotmail.com',
+    from: "recuperacaodesenhaif@hotmail.com",
     to: usuario.email,
-    subject: 'Recuperação de senha!',
-    text: 'Olá '+usuario.nome+' você tentou recuperar a senha pelo site! acesse o site http://localhost:3002/recuperacao e insira o token: '+ token +''
-    };
+    subject: "Recuperação de senha!",
+    text:
+      "Olá " +
+      usuario.nome +
+      " você tentou recuperar a senha pelo site! acesse o site http://localhost:3002/recuperacao e insira o token: " +
+      token +
+      "",
+  };
 
-    transporter.sendMail(email, (err, result)=>{
-    if(err) return console.log(err)
-    })
+  transporter.sendMail(email, (err, result) => {
+    if (err) return console.log(err);
+  });
 }
-
 
 const logar = passport.authenticate("local", {
-failureRedirect: "/login", 
-successRedirect: "/addmenu",
+  failureRedirect: "/login",
+  successRedirect: "/addmenu",
 });
 
-
-module.exports = { abreTela, cadastro, cadastrar, logar, forgot, recuperar, telatoken, atualizarsenha};
+module.exports = {
+  abreTela,
+  cadastro,
+  cadastrar,
+  logar,
+  forgot,
+  recuperar,
+  telatoken,
+  atualizarsenha,
+};
