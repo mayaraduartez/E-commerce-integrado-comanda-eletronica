fde0eb6e01bc85d8e5d1373356b0ba9b0a7af8fc
mayaraduartez <mayaraduart321@gmail.com>
2023-07-27 00:41:47 -0300

    projeto pronto

--- config/autenticacaoadmin.js
+++ config/autenticacaoadmin.js
@@ -3,7 +3,12 @@ module.exports = function(req,res,next){
   if(req.isAuthenticated()&&req.user.admin==true){
     return next();
   }
+  else if(req.isAuthenticated()&&req.user.admin==false){
+  res.redirect('/menu');
+  }
+  else {
   res.redirect('/login');
+  }
 }
 
 
